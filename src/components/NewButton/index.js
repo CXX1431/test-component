f155ac7a75072e38b7e0bf97d9de9026bb4ce300
cxx248041 <cxx248041@alibaba-inc.com>
2020-09-25 16:43:04 +0800

    update

---
+++ src/components/NewButton/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+export default function NewButton(props) {
+    return (
+        <button
+            style={{color:'red'}}
+            className="btn-style"
+        >
+          {props.name}
+        </button>
+    );
+}
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const NODE_ENV = process.env.NODE_ENV; // 获取环境变量
+const isProd = NODE_ENV === 'production';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+const plugins = isProd?[new CleanWebpackPlugin()] : [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+        template: 'public/index.html'
+    }),
+];
+
+module.exports = {
+    mode: isProd? 'production':'development',
+    entry: isProd?'./src/components/index.js':'./src/app.js',
+    output:{
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist'),
+        libraryTarget: isProd? 'commonjs2' : undefined,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use:['style-loader', 'css-loader']
+            }
+        ]
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    externals: isProd? [nodeExternals()] : [],
+    plugins,
+};
